a1e28ce4965d20b8a28ae80d4c9bc0d34b19f372
qeuete <mkkkk_21@mail.ru>
2024-05-30 15:04:04 +0300

    123

---
+++ Libraries/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Libraries
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BlueTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme("Themes/BlueTheme.xaml");
+        }
+
+        private void GreenTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme("Themes/GreenTheme.xaml");
+        }
+
+        private void ChangeTheme(string themePath)
+        {
+            var dict = new ResourceDictionary { Source = new Uri(themePath, UriKind.Relative) };
+            Application.Current.Resources.MergedDictionaries[0] = dict;
+        }
+
+        private void serializationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string textToSerialize = inputText.Text;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string jsonFilePath = Path.Combine(desktopPath, "serialized_text.json");
+            Json.SerializeToFile(textToSerialize, jsonFilePath);
+
+            inputText.Text = "";
+        }
+
+        private void deserializationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string jsonFilePath = Path.Combine(desktopPath, "serialized_text.json");
+            string deserializedText = Json.DeserializeFromFile<string>(jsonFilePath);
+            if (!string.IsNullOrEmpty(deserializedText))
+            {
+                dataLbx.Items.Add(deserializedText);
+            }
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ SavesLib/Json.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace LibraryJson
+{
+    public static class Json
+    {
+        public static void SerializeToFile<T>(T obj, string filePath)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(obj);
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine($"Сериализация успешна => {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string jsonString = File.ReadAllText(filePath);
+                    T obj = JsonSerializer.Deserialize<T>(jsonString);
+                    Console.WriteLine($"Десериализация успешна => {filePath}");
+                    return obj;
+                }
+                else
+                {
+                    Console.WriteLine($"Файл не найден: {filePath}");
+                    return default;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return default;
+            }
+        }
+    }
+}
